6016a9e5c8841b214c07aa03e968abc6c24843d2
RomanTeen1 <fquez716@gmail.com>
2023-04-04 20:41:50 -0700

    Finished url shortener

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const bodyParser = require('body-parser');
 const app = express();
+
+
+
+
+
+
+
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
@@ -14,9 +22,36 @@ app.get('/', function(req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
+const originalUrls = [];
+const shortUrls = [];
+
 // Your first API endpoint
-app.get('/api/hello', function(req, res) {
-  res.json({ greeting: 'hello API' });
+app.post('/api/shorturl', bodyParser.urlencoded({extended:false}), function(req, res) {
+  const url = req.body.url;
+  let foundIndex = originalUrls.indexOf(url);
+
+  if(!url.includes('http://') && !url.includes('https://')){
+    return res.json({error: "Invalid url"});
+  }
+
+  if(foundIndex < 0){
+    originalUrls.push(url);
+    shortUrls.push(shortUrls.length)
+    return res.json({original_url: url, short_url: shortUrls.length-1});
+  }
+  return res.json({original_url: url, short_url: shortUrls[foundIndex]});
+});
+
+app.get('/api/shorturl/:shorturl', (req, res) =>{
+  const shortUrl = Number(req.params.shorturl) ;
+  const foundIndex = shortUrls.indexOf(shortUrl);
+
+  if(foundIndex < 0){
+    return res.json({error: 'No short URL found for the given input'});
+  }
+
+  res.redirect(originalUrls[foundIndex]);
+  
 });
 
 app.listen(port, function() {
